b492e7bdb639e1a14fe4c546f626c127c7c45d84
Kushagra12 <kushagraagrawal1212@gmail.com>
2026-04-16 23:27:29 +0530

    Refactor stack implementation and fix syntax errorsq1.c

--- dsaweek1/q1.c
+++ dsaweek1/q1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h> //when you use this u can use exit(1) which forces exit from a loop which helps handles errors
 #define MAX 100
 
-struct STU {
+struct Stu {
 char id[50];
 char name[100];
 char dob[20];
@@ -10,53 +10,53 @@ float cgpa;
 };  //syntac for struct
 
 //copy pasted from stack.c code
-
 //creating push and pop function
-
 //make stack structure first
-#include <stdio.h>
-#define MAX 100
-int stack[MAX];
+//student stack
+
+struct Stu stack[MAX];
 int top = -1; //empty stack has top=-1
-int value;
+
+//initiate s
 //push function
-void push(int value)
+
+void push(struct Stu s)
 { if (top==MAX-1)
         { printf("overflow");
         return;
 }
-top++;
-stack[top]=value;
-printf("%d pushed into stack \n",value);
+stack[++top]=s;
 }
+
 //pop function
-int pop()
-{ if (top==-1) //not 0 because in stack top should be -1 for the stack to be empty
+struct Stu pop()
+{
+struct Stu temp;
+if (top==-1) //not 0 because in stack top should be -1 for the stack to be empty
         { printf("underflow");
-         return -1; }
-        top--;
-        stack[top]=value;
-        printf("%d popped into stack \n",value);
+         exit(1); }
+       temp=stack[top--];
+       return temp;
 }
 
 int main()
 {
-FILE *fp //using only 1 pointer instead of 2
-strct Stu s;
+FILE *fp; //using only 1 pointer instead of 2
+struct Stu s;
 fp=fopen("studentin.dat","r");
 if(fp==NULL)
 {
 printf("error opening input file \n");
 return 1;
 }
-while(fscaf(fp,"%s %s %s %f",s.id,s.name,s.dob,&s.cgpa)!=EOF) {
+while(fscanf(fp,"%s %s %s %f",s.id,s.name,s.dob,&s.cgpa)!=EOF) {
 push(s);
 }
 //EOF
 
 fclose(fp);
 fp=fopen("studenout.dat","w"); //w=write
-if(fp=NULL)
+if(fp==NULL)
 {printf("error opening input file \n");
 return 1;
 }
@@ -72,3 +72,4 @@ printf("%s\t %s\t %s\t %0.2f\n",s.id,s.name,s.dob,s.cgpa);
 fclose(fp);
 return 0;
 }
+
